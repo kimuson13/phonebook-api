3d349e88f54123ee225c55443d0aa53e186babb3
kimuson <kimuson13@basket.com>
2021-05-06 09:33:31 +0900

    Added gitignore ro .env

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/kimuson13/phonebook-api/phonebooks"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	phonebooks.Run()
 }
